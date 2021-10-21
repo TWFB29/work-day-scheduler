54751ce1896ddca55198dc26e342c3aa2a97d633
twfb <twfb29@yahoo.com>
2021-10-20 20:00:13 -0400

    woring on it

--- Develop/script.js
+++ Develop/script.js
@@ -2,17 +2,23 @@ var time = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(time);
 var task = $("textarea");
 var taskColor= $(".col-8");
-var currentHour;
+var currentHour = moment().hour() - 9;
 var taskArray = [];
+
 
 function setColor (){
     for (i = 0; i <= 8; i++) {
-        const timeSlot = "tsk" + i;
-        console.log(timeSlot);
+        const timeSlot = $("<textarea>");
+        const what = timeSlot.id;
+        
+        console.log(what);
+        
     }
 }
+
 // if (localStorage.getItem("localHourlyTasks")) {
 //     taskArray = JSON.parse(localStorage.getItem("localHourlyTasks"));
 // } else {
 //     taskArray = [];
 // };
+setColor();
